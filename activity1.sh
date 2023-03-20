999ae6e7edd54a1961efd62018c37580c6d942e4
srikanth dwara <dwarasree2222@gmail.com>
2023-03-19 22:17:31 -0400

    modified the activity script to prompt the message

--- activity1.sh
+++ activity1.sh
@@ -2,5 +2,11 @@
 
 instanceid=$(aws ec2 describe-instances --filters "Name=tag:Env,Values=QA,DEV" --query "Reservations[].Instances[].InstanceId
 " "Name=instance-state-name,Values=running" --output text)
+echo "${instanceid}"
+if [[ -n "${instanceid}" ]]
+then
 echo "stopping the instances ${instanceid}"
 aws ec2 stop-instances --instance-ids ${instanceid}
+else
+echo "does not have any instances matching with tag Env=QA"
+fi
