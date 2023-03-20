21f65be80e5f51895729d255a34560d4e7d49bf0
srikanth dwara <dwarasree2222@gmail.com>
2023-03-20 19:51:43 -0400

    modified and added parameters

--- activity1.sh
+++ activity1.sh
@@ -1,12 +1,22 @@
 #!bin/bash
+tag_name=$1
+tag_value=$2
 
-instanceid=$(aws ec2 describe-instances --filters "Name=tag:Env,Values=QA,DEV" --query "Reservations[].Instances[].InstanceId
+instanceid=$(aws ec2 describe-instances --filters "Name=tag:${tag_name},Values=${tag_value}" --query "Reservations[].Instances[].InstanceId
 " "Name=instance-state-name,Values=running" --output text)
+
+if [[ -z "${tag_name}" ]]; then
+tag_name="Env"
+fi
+if [[ -z "${tag_value}" ]]; then
+tag_value="QA"
+fi
+
 echo "${instanceid}"
-if [[ -n "${instanceid}" ]]
+if [[ -n "${instanceid}" ]];
 then
 echo "stopping the instances ${instanceid}"
 aws ec2 stop-instances --instance-ids ${instanceid}
 else
-echo "does not have any instances matching with tag Env=QA"
+echo "does not have any instances matching with tag ${tag_name}=${tag_value}"
 fi
